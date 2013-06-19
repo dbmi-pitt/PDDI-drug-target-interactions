e6cd76760509710bbbd911a5c5450cea7e7877ba
Richard D Boyce, PhD <rkboyce@users.noreply.github.com>
2013-06-19 10:58:24 +0000

    playing with the pharmgkb dictionare

---
+++ jeremy-sandbox/Gene List/dictWriterModified.py
@@ -0,0 +1,55 @@
+# dictWriterModified.py
+#
+# Read the Phamgkb gene file and store relevant data in a dictionary
+# then pickle the dictionary for later usexs
+#
+# Jeremy Jao, Dr. Richard Boyce
+# University of Pittsburgh: DBMI
+# 6/19/2013
+# 
+
+
+import csv
+import sys
+import cPickle
+
+csv.field_size_limit(sys.maxint)
+
+############################## GLOBALS ##############################
+class PharmGKBDict:
+    
+    def __init__(self):
+        self.data = {}
+    
+    
+        with open('genes.tsv', 'rb') as f:
+            reader = csv.reader(f, delimiter='\t', quoting=csv.QUOTE_NONE)
+            for i, row in enumerate(reader):
+                if i == 0:
+                    pharmID = row[0]
+                    entrezID = row[1]
+                    ensembleID = row[2]
+                    name = row[3]
+                    alternateNames = row[5]
+                    alternateSymbols = row[6]
+                    isVIP = row[7]
+                    hasVariantAnnotation = row[8]
+                    reference = row[9]
+                    cpicGuide = row[10]
+                else:
+                    referenceString = row[9].split(',')
+                    referenceList = []
+                    for col in referenceString:
+                        referenceList.append(col)
+                    
+                    altSymbString = row[6].split(',')
+                    altSymbolsList = []
+                    for col in altSymbString:
+                        altSymbolsList.append(col)
+                
+                    e = {pharmID: row[0], entrezID: row[1], ensembleID: row[2], name: row[3], alternateNames: row[5], alternateSymbols: altSymbolsList, isVIP: row[7], hasVariantAnnotation: row[8], reference: referenceList, cpicGuide: row[10]}
+                    self.data.update({row[4]: e})
+
+    def writeData(path):
+        with open(path, 'w') as f:
+            cPickle.dump(d, f)
